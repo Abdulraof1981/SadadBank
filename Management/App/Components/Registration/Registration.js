094581f2e77ff688ed93bcbbfc2cede5a878919e
T.mahmudi <T.mahmudi@ISPRD-MI03>
2019-09-17 14:33:07 +0200

    add regestration filtirs, select bulk actions and so on ..

--- Management/App/Components/Registration/Registration.js
+++ Management/App/Components/Registration/Registration.js
@@ -44,12 +44,33 @@ export default {
             Customers: [],
             state: 0,
             Search: null,
-            selectAll: false,
             pageNumbers: 10,
-            transDate : null
+            transDate: null,
+            selectAllArray: [],
+            
+            dialogVisible: false,
+            ruleForm: {
+                desc: '',
+            },
+            rules: {
+                desc: [
+                    { required: true, message: 'الرجاء ادخال سبب الرفض', trigger: 'blur' },
+                ],
+            },
+            bankActionIdToReject: ''
         };
     },
     methods: {
+        cahngeBankActionIdToReject(BankActionId) {
+            this.bankActionIdToReject = BankActionId;
+            this.ruleForm.desc = '';
+            this.dialogVisible = true;
+        },
+        
+        beforeClose() {
+            this.dialogVisible = false;
+        },
+
         LastConfirm(BankActionId) {
             this.$confirm('هل انت متأكد من التأكيد النهائي لهذه الحركة؟', 'تـحذير', {
                 confirmButtonText: 'نـعم',
@@ -89,58 +110,35 @@ export default {
         },
 
         LastConfirmAll() {
-            this.$blockUI.Start();
-            this.Customers.forEach(function (element) {
-                if (element.checkbox) {
-                    if (element.status != 2) {
+            if (this.selectAllArray.length == 0) {
+                this.$message({
+                    type: 'error',
+                    dangerouslyUseHTMLString: true,
+                    message: '<strong>' + 'يجب تحديد الزبائن المراد إجراء تأكيد نهائي لهم أولاً' + '</strong>'
+                });
+                return;
+            }
+
+            for ( var i=0 ; i<this.Customers.length ; i++ ){
+                if (this.Customers[i].checkbox) {
+                    if (this.Customers[i].status != 2 ) {
                         this.$message({
                             type: 'error',
                             dangerouslyUseHTMLString: true,
-                            message: '<strong>' + 'يجب تحديد الحركات التي تحتاج إلى تأكيد نهائي فقط، الرجاء إعادة المحاولة!' + '</strong>'
+                            message: '<strong>' + 'يجب تحديد الزبائن الذين يحتاجون إلى تأكيد نهائي فقط، الرجاء إعادة المحاولة!' + '</strong>'
                         });
-                        this.$blockUI.Stop();
-                    }
-                    else
-                    {
-                        this.$http.LastConfirm(element.BankActionId)
-                        .then(response => {
-                            //this.$blockUI.Stop();
-                            /*this.GetCustomers(this.pageNo);
-                            if (response.data.code == 0) {
-                                this.$message({
-                                    type: 'info',
-                                    dangerouslyUseHTMLString: true,
-                                    message: '<strong>' + response.data.message + '</strong>'
-                                });
-                            } else if (response.data.code == 1) {
-                                this.$message({
-                                    type: 'info',
-                                    dangerouslyUseHTMLString: true,
-                                    message: '<strong> Warning: ' + response.data.message + '</strong>'
-                                });
-                            } else {
-                                this.$message({
-                                    type: 'error',
-                                    dangerouslyUseHTMLString: true,
-                                    message: '<strong>' + response.data.message + '</strong>'
-                                });
-                            }*/
-                        }).catch((err) => {
-                            /*this.$blockUI.Stop();
-                            console.error(err);
-                            this.pages = 0;*/
-                        });
+                        return;
                     }
                 }
-            });
-
+            }
+            
             this.$confirm('هل انت متأكد من التأكيد النهائي لجميع الحركات المحددة؟', 'تـحذير', {
                 confirmButtonText: 'نـعم',
                 cancelButtonText: 'لا',
                 type: 'warning'
             }).then(() => {
                 this.$blockUI.Start();
-                this.$http.LastConfirm(BankActionId)
+                this.$http.LastConfirmAll(this.selectAllArray)
                     .then(response => {
                         this.$blockUI.Stop();
                         this.GetCustomers(this.pageNo);
@@ -170,41 +168,45 @@ export default {
                     });
             });
         },
+        
+        RejectCustomer() {
+            this.$refs['ruleForm'].validate((valid) => {
+                if (valid) {
 
-
-        RejectCustomer(BankActionId) {
-            this.$confirm('هل تريد رفض هذه الحركة؟', 'تـحذير', {
-                confirmButtonText: 'نـعم',
-                cancelButtonText: 'لا',
-                type: 'warning'
-            }).then(() => {
-                this.$blockUI.Start();
-                this.$http.RejectCustomer(BankActionId)
-                    .then(response => {
-                        this.$blockUI.Stop();
-                        this.GetCustomers(this.pageNo);
-                        this.$message({
-                            type: 'info',
-                            dangerouslyUseHTMLString: true,
-                            message: '<strong>' + 'تم رفض الحركة بنجاح' + '</strong>'
+                    this.$blockUI.Start();
+                    this.$http.RejectCustomer(this.bankActionIdToReject, this.ruleForm.desc)
+                        .then(response => {
+                            this.$blockUI.Stop();
+                            this.GetCustomers(this.pageNo);
+                            this.$message({
+                                type: 'info',
+                                dangerouslyUseHTMLString: true,
+                                message: '<strong>' + 'تم رفض الحركة بنجاح' + '</strong>'
+                            });
+                            this.dialogVisible = false;
+                        })
+                        .catch((err) => {
+                            this.$blockUI.Stop();
+                            console.error(err);
+                            this.pages = 0;
+                            this.dialogVisible = false;
                         });
-                    })
-                    .catch((err) => {
-                        this.$blockUI.Stop();
-                        console.error(err);
-                        this.pages = 0;
-                    });
+                
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
             });
         },
         AddCustomers() {
             this.state = 1;
         },
 
-        formatDate(date) {
+        formatDate(date, i) {
             if (date == null)
                 return null;
 
-            var d = new Date(date),
+            var d = new Date(date[i]),
                 month = '' + (d.getMonth() + 1),
                 day = '' + d.getDate(),
                 year = d.getFullYear();
@@ -218,6 +220,9 @@ export default {
         },
 
         GetCustomers(pageNo) {
+            this.selectAllArray = [];
+            this.Customers = [];
+
             this.pageNo = pageNo;
             if (this.pageNo === undefined) {
                 this.pageNo = 1;
@@ -226,7 +231,7 @@ export default {
                 this.Search = "";
             }
             this.$blockUI.Start();
-            this.$http.GetCustomers(this.pageNo, this.pageSize, this.Search, this.Status, this.formatDate(this.transDate))
+            this.$http.GetCustomers(this.pageNo, this.pageSize, this.Search, this.Status, this.formatDate(this.transDate, 0), this.formatDate(this.transDate, 1))
                 .then(response => {
                     this.$blockUI.Stop();
                     this.Customers = response.data.customers;
@@ -243,27 +248,27 @@ export default {
         },
 
         check(index) {
-            if (index === -1) {
-                var el = document.querySelectorAll('.checkboxBtn input[type=checkbox]');
-                //el[5].checked = true;
-                //el[5].click();
-                console.log(el);
-                //console.log(el[5].checked);
-                /*for (var i = 0; i < this.Customers.length; i++) {
-                    this.Customers[i].checkbox = !this.selectAll;
-                    el[i].checked = !this.selectAll;
-                }*/
-            }
-            else {
-                this.Customers[index].checkbox = !this.Customers[index].checkbox;
-            }
-            console.log(this.Customers);
+            this.Customers[index].checkbox = !this.Customers[index].checkbox;
+            if ( this.Customers[index].checkbox )
+                this.selectAllArray.push(this.Customers[index].bankActionId);
+            else
+                this.selectAllArray = this.selectAllArray.filter(item => item !== this.Customers[index].bankActionId);
         },
 
         changePageNumbers(pageNumbers) {
             this.pageSize = pageNumbers;
             this.GetCustomers(this.pageNo);
         }
+
+        , resetFields() {
+            this.Status= '';
+            this.pageNo= 1;
+            this.pages = 0;
+            this.pageNumbers = 10;
+            this.Search= null;
+            this.transDate= null;
+            this.selectAllArray = [];
+            this.changePageNumbers(this.pageNumbers);
+        }
     }
 }
-    ;
